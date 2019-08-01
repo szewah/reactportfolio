615e5d85509d0ec6ff01bcc68678ec1d67455a44
szewah <swgrleong@gmail.com>
2019-07-31 20:52:16 -0400

    contact page layout

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import IntroPage from './pages/Intro'
 import AboutPage from './pages/About'
 import PortfolioPage from './pages/Portfolio';
-// import ContactPage from './pages/Contact';
+import ContactPage from './pages/Contact';
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
         <IntroPage></IntroPage>
         <AboutPage></AboutPage>
         <PortfolioPage></PortfolioPage>
-        {/* <ContactPage></ContactPage> */}
+        <ContactPage></ContactPage>
     </div>
   );
 }
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,1 +1,22 @@
 import React from 'react';
+
+
+function ContactPage () {
+    return (
+        <div className="contacts">
+            <h3 className="contactH3">CONTACT</h3>
+            <h4 className="contactText">How can I help you today?</h4>
+            <a href="https://www.linkedin.com/in/sze-wah-chin-%E7%A7%A6%E6%80%9D%E5%8D%8E-87a14a14/"  target="_blank">
+                <i class="fab fa-linkedin-in"></i>
+            </a>
+            <a href="https://twitter.com/szewahleong" target="_blank">
+                <i class="fab fa-twitter"></i>
+            </a>
+            <a href="mailto:szewahchin@gmail.com">
+            <i class="fas fa-envelope-open-text"></i>
+            </a>
+        </div>
+    )
+}
+
+export default ContactPage;
