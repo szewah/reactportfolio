c43f7bc4e267a6ef698aeca18e2fea78999bc175
szewah <swgrleong@gmail.com>
2019-08-02 13:45:20 -0400

    added methods to onclick

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -12,19 +12,21 @@ class NavBar extends Component {
     handleOnClick = event => {
         event.preventDefault();
         this.setState({clicked:true});
+        alert(event.target);
+        //if else statements here?
     }
 
     render(){
-        // if else statements 
+        // if else statements here?
         return (
             <Navbar sticky="top" collapseOnSelect expand='lg' bg='dark' variant='dark'>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="mr-auto">
-                    <Nav.Link href="#introPage">Home</Nav.Link>
-                    <Nav.Link href="#about">About</Nav.Link>
-                    <Nav.Link href="#portfolio">Portfolio</Nav.Link>
-                    <Nav.Link href="#contacts">Contact</Nav.Link>
+                    <Nav.Link href="#introPage" onClick={this.handleOnClick}>Home</Nav.Link>
+                    <Nav.Link href="#about" onClick={this.handleOnClick}>About</Nav.Link>
+                    <Nav.Link href="#portfolio" onClick={this.handleOnClick}>Portfolio</Nav.Link>
+                    <Nav.Link href="#contacts" onClick={this.handleOnClick}>Contact</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
             </Navbar>
