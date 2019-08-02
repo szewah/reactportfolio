43431a29608c0584b0659f690632f3a600946af2
szewah <swgrleong@gmail.com>
2019-08-02 13:32:00 -0400

    navbar links

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,21 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Navbar, Nav} from 'react-bootstrap';
 import './style.css';
 
-const NavBar = props => {
-    return (
-        <Navbar sticky="top" collapseOnSelect expand='lg' bg='dark' variant='dark'>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="mr-auto">
-                <Nav.Link href="#introPage">Home</Nav.Link>
-                <Nav.Link href="#about">About</Nav.Link>
-                <Nav.Link href="#portfolio">Portfolio</Nav.Link>
-                <Nav.Link href="#contacts">Contact</Nav.Link>
-            </Nav>
-        </Navbar.Collapse>
-        </Navbar>
-    )
+class NavBar extends Component {
+
+    state ={
+
+    }
+
+    render(){
+        return (
+            <Navbar sticky="top" collapseOnSelect expand='lg' bg='dark' variant='dark'>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link href="#introPage">Home</Nav.Link>
+                    <Nav.Link href="#about">About</Nav.Link>
+                    <Nav.Link href="#portfolio">Portfolio</Nav.Link>
+                    <Nav.Link href="#contacts">Contact</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+            </Navbar>
+        )
+    }
 }
 
 export default NavBar
